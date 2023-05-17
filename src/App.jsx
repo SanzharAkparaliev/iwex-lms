9da6a6af8dd195a6881ae3745c08247123bcef4e
Ilham Temirgaliev <ilham2004t@gmail.com>
2023-05-17 22:52:19 +0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
    <Layout>
 <Routes>
   <Route path='/' element={<Home/>}/>
-  <Route path='login' element={<Login/>}/>
+  <Route path='/login' element={<Login/>}/>
 </Routes>
    </Layout>
   )
