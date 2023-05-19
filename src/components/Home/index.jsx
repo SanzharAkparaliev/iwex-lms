6a0f9b27c958c3dd7cf59b1b9c162c515d6c9f67
Ilham Temirgaliev <ilham2004t@gmail.com>
2023-05-19 14:53:36 +0600

    fotter comit

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import AboutContent from '../AboutContent/AboutContent';
 import BannerContent from '../Banner';
 import { Footer } from '../Footer/Footer';
+import { Teachers } from '../Teachers/Teachers';
 
 import styles from './home.module.css';
 
@@ -8,7 +9,7 @@ const HomePage = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrappper_container}>
-       
+        <Teachers/>
       </div>
       <Footer/>
     </div>
---
+++ src/components/Teachers/Teachers.jsx
@@ -0,0 +1,8 @@
+import React from 'react' 
+import styled from './Teachers.module.css'
+
+export const Teachers = () => {
+  return (
+    <div className={styled.wrap}>Teachers</div>
+  )
+}
