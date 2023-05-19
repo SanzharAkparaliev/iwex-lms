618294cda6e776ba92a49372aa5e0d235c2a2ed1
Islam Temirgaliev <islam2004tr@mail.ru>
2023-05-19 15:17:40 +0600

    commit Techers

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,8 +3,7 @@ import BannerContent from '../Banner';
 
 import ChooseUs from '../ChooseUs';
 
-import { Footer } from '../Footer/Footer';
-import { Teachers } from '../Teachers/Teachers';
+
 
 
 import styles from './home.module.css';
