c89426093b8a865c9148f3779526c22b1f9e1ae0
Ilham Temirgaliev <ilham2004t@gmail.com>
2023-05-19 15:19:40 +0600

    sadf

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,8 +3,6 @@ import BannerContent from '../Banner';
 
 import ChooseUs from '../ChooseUs';
 
-import { Footer } from '../Footer/Footer';
-import { Teachers } from '../Teachers/Teachers';
 
 
 import styles from './home.module.css';
