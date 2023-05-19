7fb03984b3ec3eefff81ece07a79eb943549c24b
sweetkurut <devxwc@gmail.com>
2023-05-19 16:27:53 +0600

    fixed videoplayer

--- src/components/ChooseUs/Video/index.jsx
+++ src/components/ChooseUs/Video/index.jsx
@@ -5,20 +5,12 @@ const VideoPlayer = () => {
   const videoRef = useRef(null);
   const [showVideo, setShowVideo] = useState(false);
 
-  const togglePlay = () => {
-    const video = videoRef.current;
-    if (video.paused) {
-      video.play();
-    } else {
-      video.pause();
-    }
-    setShowVideo(!video.paused);
-  };
 
   return;
   <div className={styles.video_img}>
-    <video ref={videoRef} src="https://www.youtube.com/watch?v=IqAjdbgzMV8" />
-    <button onClick={togglePlay}>{showVideo ? 'Pause' : 'Play'}</button>
+    <video width="100%" height="100%" controls>
+
+    </video>
   </div>;
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import styles from './navbar.module.css';
 
+
 import { BiLogIn } from "react-icons/bi";
 const Navbarr = () => {
+
   return (
     <div className={styles.conteiner}>
       <div className={styles.blok}>
         <div className={styles.contlogo}>
+
           {/* <img className={styles.img} src={logo1} alt="" />
          */}
+
           <p className={styles.parag}>LMS</p>
         </div>
         <nav className={styles.contlink}>
