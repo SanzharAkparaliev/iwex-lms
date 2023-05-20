c0571b1cd571a84352068f122ac1d38a7a87324f
sweetkurut <devxwc@gmail.com>
2023-05-20 21:44:39 +0600

    added fature-section

--- src/components/ChooseUs/index.jsx
+++ src/components/ChooseUs/index.jsx
@@ -2,44 +2,48 @@ import { useState } from 'react';
 import styles from '../Home/home.module.css';
 import VideoPlayer from './Video';
 import { FaPlayCircle } from 'react-icons/fa';
+import FestureSection from '../FeatureSection';
 
 const ChooseUs = () => {
   const [showVideo, setShowVideo] = useState(false);
 
   return (
-    <article className={styles.article}>
-      <div className={styles.article_blog}>
-        <div className={styles.article_desc}>
-          <h2 className={styles.article_title}>Why Choose Us</h2>
-          <p className={styles.article_description}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, illo
-            natus consectetur labore sint voluptatem odio explicabo. Laboriosam
-            consectetur, dignissimos, minus, vero nulla ab maxime quisquam
-            quidem delectus perferendis saepe? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Nulla quae iure asperiores soluta? Sit
-            iure ullam velit tempora eius, obcaecati officiis hic pariatur,
-            earum magni, architecto fugiat magnam quo assumenda.
-          </p>
+    <>
+      <article className={styles.article}>
+        <div className={styles.article_blog}>
+          <div className={styles.article_desc}>
+            <h2 className={styles.article_title}>Why Choose Us</h2>
+            <p className={styles.article_description}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis,
+              illo natus consectetur labore sint voluptatem odio explicabo.
+              Laboriosam consectetur, dignissimos, minus, vero nulla ab maxime
+              quisquam quidem delectus perferendis saepe? Lorem, ipsum dolor sit
+              amet consectetur adipisicing elit. Nulla quae iure asperiores
+              soluta? Sit iure ullam velit tempora eius, obcaecati officiis hic
+              pariatur, earum magni, architecto fugiat magnam quo assumenda.
+            </p>
+          </div>
+          <div className={styles.article_video_blog}>
+            {showVideo ? (
+              <VideoPlayer />
+            ) : (
+              <img
+                className={styles.subImage}
+                src="https://courses.iwex.kg/pluginfile.php/13123/block_cocoon_about_1/content/aaaa.jpg"
+                alt=""
+              />
+            )}
+            {!showVideo && (
+              <FaPlayCircle
+                className={styles.play_icon}
+                onClick={() => setShowVideo(!showVideo)}
+              />
+            )}
+          </div>
         </div>
-        <div className={styles.article_video_blog}>
-          {showVideo ? (
-            <VideoPlayer />
-          ) : (
-            <img
-              className={styles.subImage}
-              src="https://courses.iwex.kg/pluginfile.php/13123/block_cocoon_about_1/content/aaaa.jpg"
-              alt=""
-            />
-          )}
-          {!showVideo && (
-            <FaPlayCircle
-              className={styles.play_icon}
-              onClick={() => setShowVideo(!showVideo)}
-            />
-          )}
-        </div>
-      </div>
-    </article>
+      </article>
+      <FestureSection />
+    </>
   );
 };
 
