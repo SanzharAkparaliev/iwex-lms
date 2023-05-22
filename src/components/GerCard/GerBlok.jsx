3dd46f0d5a0b989c446ad953585e9ba52f1c489f
Islam Temirgaliev <islam2004tr@mail.ru>
2023-05-22 15:05:59 +0600

    commit cardGer

--- src/components/GerCard/GerBlok.jsx
+++ src/components/GerCard/GerBlok.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
  import styles from './GerCard.module.css'
 import CardG from '../cardG/CardG'
+import { DataGer } from '../GercardData';
+
+
 const GerBlok = () => {
+  
+   const  data = DataGer
+console.log(data)
+
   return (
     <div className={styles.conteiner}>
       <div className={styles.cardBlok}>
-      <CardG/>
-      <CardG/>
-      <CardG/>
+  {
+    data.map((item ) => {
+return <CardG key={item.id} name={item.name} lesson = {item.leson} people = {item.people}/>
+    })
+  }
+      
       </div>
     </div>
   )
--- src/components/cardG/CardG.jsx
+++ src/components/cardG/CardG.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import styles from './cardg.module.css'
 import img from './flag.jpg'
-const CardG = () => {
+import { IoBookSharp } from "react-icons/io5";
+import { MdPeopleAlt } from "react-icons/md";
+
+
+
+const CardG = ({name ,lesson , people}) => {
   return (
     <div className={styles.box}>
         <div className={styles.flagBox}>
             <img className={styles.imgGer} src={img} alt="" />
         </div>
+      <h2 className={styles.name}>{name}</h2>
+      <div className={styles.boxlesPepl}>
+<div className={styles.lessonsBlok}>
+<i className={styles.iconBook}><IoBookSharp/></i>
+<p className={styles.leson}>{lesson}</p>
+</div>
+<div className={styles.lessonsBlok}>
+<i className={styles.iconBook}><MdPeopleAlt/></i>
+<p className={styles.leson}>{people}k</p>
+</div>
+      </div>
     </div>
   )
 }
