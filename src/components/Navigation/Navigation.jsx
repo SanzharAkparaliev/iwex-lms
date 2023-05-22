fa71e8256608e67faab33666db2e15301068a577
sweetkurut <devxwc@gmail.com>
2023-05-22 13:23:44 +0600

    SDCSDC

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,12 +3,9 @@ import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
 import cn from 'clsx';
 import { BiLogIn } from 'react-icons/bi';
-<<<<<<< HEAD
-=======
 import { RxHamburgerMenu, RxCross1 } from 'react-icons/rx';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
->>>>>>> 283f2cfe3e3628d1e53b776361c235ddbc459815
 import logo1 from './logo3.png';
 import './habmerger.css';
 
@@ -36,30 +33,6 @@ const Navigation = () => {
           <p className={styles.parag}>LMS</p>
         </div>
 
-<<<<<<< HEAD
-        <button
-          className="hamburger hamburger--spring "
-          onClick={hendler}
-          type="button"
-        >
-          <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
-          </span>
-        </button>
-
-        <div className={cn(styles.nav, show && styles.active)}>
-          <NavLink className={styles.join} to={'/'}>
-            Home
-          </NavLink>
-          <NavLink className={styles.join} to={'/tech'}>
-            Techers
-          </NavLink>
-          <NavLink className={styles.join} to={'/login'}>
-          <NavLink className={styles.join} to={'/profile'}>
-            Profile
-          </NavLink>
-            {<BiLogIn />}
-=======
         <button className={styles.hamburger} onClick={hendler} type="button">
           {show ? (
             <AiOutlineClose className={styles.iconCloss} />
@@ -97,18 +70,15 @@ const Navigation = () => {
             to={'/login'}
           >
             <label className={styles.lab}>login </label>
->>>>>>> 283f2cfe3e3628d1e53b776361c235ddbc459815
           </NavLink>
         </div>
       </div>
     </div>
   );
 };
-<<<<<<< HEAD
 
-=======
 {
   /* <i className={styles.icon2}>{<BiLogIn/>} </i> */
 }
->>>>>>> 283f2cfe3e3628d1e53b776361c235ddbc459815
+
 export default Navigation;
