14983f9cadc63b12e42803f0ed4b087f9f5f8c78
Islam Temirgaliev <islam2004tr@mail.ru>
2023-05-23 15:47:23 +0600

    commit

--- src/components/TaskWork/Taskwork.jsx
+++ src/components/TaskWork/Taskwork.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import styled from './Taskwork.module.css'
+import React from 'react';
+import styled from './Taskwork.module.css';
 const Taskwork = () => {
   return (
     <div className={styled.wraper}>
+      <div className={styled.block2}>
+        <div className={styled.item}>
+          <input
+            className={styled.input}
+            type="checkbox"
+            name="inp"
+            id="labe"
+          />
+          <label htmlFor="labe" className={styled.label}>
+            Lessons
+          </label>
+          <div className={styled.text_f}>
+            <p className={styled.p}>
+              {' '}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+              vel! Praesentium vero atque aliquam impedit aliquid natus cum
+              distinctio beatae sint incidunt aspernatur, tenetur, ipsam amet ab
+              doloremque vel quae! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Voluptatibus delectus itaque ab tenetur dolor
+              assumenda repellendus fugit, nulla numquam! Facere nobis
+              voluptates optio quos illo iure aut fugiat blanditiis. Deleniti?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel
+              quis, tempore, maxime nobis provident animi, aspernatur recusandae
+              aperiam magni quia dicta. Quae provident perferendis debitis? Cum
+              eum veniam aut laudantium.
+            </p>
+          </div>
+        </div>
+      </div>
 
-    
-<div className={styled.block2}> 
-    <div className={styled.item}>
-                     <input className={styled.input} type="checkbox" name='inp' id='labe' />
-                    <label htmlFor='labe' className={styled.label}>Lessons</label>
-                    <div className={styled.text_f}>
-                        <p className={styled.p}> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel quis, tempore, maxime nobis provident animi, aspernatur recusandae aperiam magni quia dicta. Quae provident perferendis debitis? Cum eum veniam aut laudantium.</p>
-    
-                    </div>
-                </div>
-    
-</div>
-
-    {/* <div className={styled.block1}>
+      {/* <div className={styled.block1}>
         <div className={styled.title}>
             <h1 className={styled.text}>Часто задоваемые вопросы</h1>
         </div>
     </div> */}
-    {/* <div className={styled.block2}>
+      {/* <div className={styled.block2}>
        <div className={styled.yy}>
            
             <div className={styled.item}>
@@ -59,8 +75,8 @@ const Taskwork = () => {
             </div>
        </div>
     </div> */}
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default Taskwork
+export default Taskwork;
