b3e12097b941a1c093eee91340feedf4a947f0a1
Islam Temirgaliev <islam2004tr@mail.ru>
2023-06-01 11:12:31 +0600

    com

--- src/components/Lessons/Lessons.jsx
+++ src/components/Lessons/Lessons.jsx
@@ -3,6 +3,7 @@ import Taskwork from './../TaskWork/Taskwork';
 
 import styles from './Lessons.module.css'
 import { useNavigate, useParams } from 'react-router';
+import AddLesons from '../AddLeson';
 
 
 const Lessons = () => {
@@ -10,25 +11,25 @@ const Lessons = () => {
  const params =  useParams()
 
  const  hendRouter = () => {
-  navigate(`/corses/:id/lessons`)
+  navigate(`/corses/:${params.id}/lessons`)
 //  реализуем айдишник каторы получили с парамс , вызываем функцыю сетегого запроса на получение 
 //  информацый адного конкретного курса по айди пробвурасывая в функцыю этот самы айди.
 // получаяем в стейт всю инфу о конкретном курсе  и вместе  снимм же масив с уроками каторы мы перебераем мэпом 
 // и возрощяем таскворк а просто раставим все ключи по местам 
- 
+ }
   return (
     <div className={styles.conteiner}>
       <h1 className={styles.title}>Lessons</h1>
       <div className={styles.blok_button}>
         <button onClick={hendRouter} className={styles.addButton}>Add Lessons</button>
       </div>
-      <AddLesons />
+    
     <Taskwork/>
     </div>
       
     )
 
- }
-}
+ }; export default Lessons
+
 
 
