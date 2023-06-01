0556a7072b45d292224036437db0cf6d0998c35a
Islam Temirgaliev <islam2004tr@mail.ru>
2023-06-01 15:58:53 +0600

    com add corses

--- src/components/CoursesPages/CoursesPages.jsx
+++ src/components/CoursesPages/CoursesPages.jsx
@@ -5,11 +5,57 @@ import CardCourses from '../CardCourses/CardCourses';
 import { Footer } from '../Footer/Footer';
 import Cookies from 'js-cookie';
 
+import 'react-quill/dist/quill.snow.css';
+import EditorToolbar, { modules, formats } from '../AddLeson/EditorToolbar';
+import '../../App.css';
+import ReactQuill from 'react-quill';
+
+
+
 const CoursesPages = () => {
-  useState
+  
+
+  const [selectedOption, setSelectedOption] = useState('');
+  const [state, setState] = useState();
+
+  const handleChange = (event) => {
+    selectedOption({ value });
+  };
+  
   const data = CoursesData;
   return (
     <div className={styles.conteiner}>
+
+<div className={styles.input_wrapper}>
+  
+  <div className={styles.input_group}>
+            <input className={styles.input} placeholder="Название Курса" />
+          </div>
+  <div className={styles.input_group}>
+            <input className={styles.input} placeholder="описание" />
+          </div>
+          <div className={styles.input_group}>
+            {/* <textarea className={styles.input} placeholder="content"></textarea> */}
+            {/* <ReactQuill theme="snow" /> */}
+            <EditorToolbar />
+            <ReactQuill
+              theme="snow"
+              // value={state.value}
+              onChange={handleChange}
+              placeholder={'Write something awesome...'}
+              modules={modules}
+              formats={formats}
+              className={styles.editor}
+              name="editor"
+            />
+          </div>
+          <div className={styles.blokk}>
+            <button className={styles.btn}>Добавить</button>
+          </div>
+  
+  
+</div>
+
       <div className={styles.cardBlok}>
         {Cookies.get('token') &&
           data.map((item) => {
