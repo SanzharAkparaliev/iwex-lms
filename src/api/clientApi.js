c6d15d98d34d2ed0ce9c7dff2518357218940ea5
sweetkurut <devxwc@gmail.com>
2023-06-02 16:31:10 +0600

    fixed env file

--- src/api/clientApi.js
+++ src/api/clientApi.js
@@ -1,20 +1,9 @@
 import ky from 'ky';
 
 const requestApi = ky.create({
-  prefixUrl: import.meta.env.USER_AUTH,
+  prefixUrl: import.meta.env.VITE_USER_AUTH,
 });
 
-// export const authUser =
-
-// export const registerUser = (data) => {
-//   return requestApi.post(
-//     '/api/v1/auth/login',
-//     JSON.stringify({ email: user, password: pwd })
-//   );
-// };
-
 export const userAuth = (data) => {
-  return requestApi
-    .post('http://146.190.46.127:8080/api/v1/auth/login', { json: data })
-    .json();
+  return requestApi.post('v1/auth/login', { json: data }).json();
 };
--- src/components/Materials/index.jsx
+++ src/components/Materials/index.jsx
@@ -1,7 +1,6 @@
 import styles from './material.module.css';
 
 const Material = () => {
-  return <div>Страница Материалов курса</div>;
+  return <div className={styles.wrapper}></div>;
 };
-
 export default Material;
