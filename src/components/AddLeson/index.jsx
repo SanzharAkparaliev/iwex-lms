faa036de1bfc2fb414bcfe0b5264ae1b75593fd5
sweetkurut <devxwc@gmail.com>
2023-06-05 14:45:48 +0600

    refresh usestate

--- src/components/AddLeson/index.jsx
+++ src/components/AddLeson/index.jsx
@@ -6,15 +6,25 @@ import EditorToolbar, { modules, formats } from './EditorToolbar';
 import '../../App.css';
 
 const AddLesons = () => {
-  const [selectedOption, setSelectedOption] = useState('');
+  // const [selectedOption, setSelectedOption] = useState('');
   const [state, setState] = useState();
+  const [input, setInput] = useState();
 
-  const handleChange = (event) => {
-    selectedOption({ value });
-  };
-  // const handleChange = (value) => {
-  //   setState({ value });
+  // const handleChange = (event) => {
+  //   setSelectedOption(value);
   // };
+  const handleChange = (value) => {
+    setState({ value });
+  };
+
+  const handleInput = (e) => {
+    setInput((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
   return (
     <div className={styles.input_wrapper}>
@@ -25,14 +35,26 @@ const AddLesons = () => {
             onChange={handleChange}
             className={styles.selectStyle}
           >
-            <option value="">Выберите урок</option>
-            <option value="option1">Опция 1</option>
-            <option value="option2">Опция 2</option>
-            <option value="option3">Опция 3</option>
+            <option name="changeLesson" value="" onChange={handleInput}>
+              Выберите урок
+            </option>
+            <option name="lesson01" value="option1" onChange={handleInput}>
+              Опция 1
+            </option>
+            <option name="lesson02" value="option2" onChange={handleInput}>
+              Опция 2
+            </option>
+            <option name="lesson03" value="option3" onChange={handleInput}>
+              Опция 3
+            </option>
           </select>
         </div>
         <div className={styles.input_group}>
-          <input className={styles.input} placeholder="Название урока" />
+          <input
+            className={styles.input}
+            placeholder="Название урока"
+            onChange={handleInput}
+          />
         </div>
         <div className={styles.input_group}>
           {/* <textarea className={styles.input} placeholder="content"></textarea> */}
